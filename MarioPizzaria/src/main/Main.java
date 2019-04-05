7710b723bbebf6b2925e7b8681ba62a9a9b63260
Bruger <Bruger@10.50.137.232>
2019-04-05 11:07:06 +0200

    Made the github

---
+++ MarioPizzaria/src/main/Main.java
@@ -0,0 +1,17 @@
+
+package main;
+
+/**
+ *
+ * @author Michael N. Korsgaard
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("New project");
+        
+    }
+    
+}
