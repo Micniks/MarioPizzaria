86aeaf6019c1da3e23f46876f73f2401666e44a3
Bruger <Bruger@10.50.131.95>
2019-04-08 14:45:52 +0200

    Forsøg på at oploade til projektet

--- MarioPizzaria/src/main/Main.java
+++ MarioPizzaria/src/main/Main.java
@@ -1,5 +1,10 @@
-
 package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -7,11 +12,27 @@ package main;
  */
 public class Main {
 
-    
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+
         System.out.println("New project");
+
         
+        //Til DatabaseFacade
+        String user = "root";
+        String password = "Gunstar1";
+        String IP = "localhost";
+        String PORT = "3306";
+        String DATABASE = "mario";
+        String serverTime = "serverTimezone=UTC";
+        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE+"?"+serverTime;
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery("SELECT * FROM Pizza");
+        while (result.next()) {
+            int resultat = result.getInt("pizzaNo");
+            System.out.println(resultat);
+        }
+
     }
-    
+
 }
