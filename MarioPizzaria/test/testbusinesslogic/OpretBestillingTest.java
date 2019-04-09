d4edd2761da1a03a2c2b7fcefff7d8300503258d
Bruger <Bruger@10.50.131.95>
2019-04-09 11:11:45 +0200

    Update test

--- MarioPizzaria/test/testbusinesslogic/OpretBestillingTest.java
+++ MarioPizzaria/test/testbusinesslogic/OpretBestillingTest.java
@@ -28,7 +28,7 @@ public class OpretBestillingTest {
 
         //act
         ctrl.newOrder();
-        
+
         //assert
         assertTrue(ui.output.get(0).contains("Skriv hvilket pizzanummer kunden har bestilt"));
         assertTrue(ui.output.get(1).contains("Skriv hvor mange af pizzaen kunde har bestilt"));
@@ -40,35 +40,86 @@ public class OpretBestillingTest {
         assertEquals("Amerikaner", ctrl.getActiveOrders().get(0).getPizzaList().get(0).getPizzaName());
     }
 
-    /*
     @Test
-    public void opretBestillingToPizza() {
+    public void opretBestillingToAfSammePizza() {
         ArrayList<Pizza> menukort = new ArrayList();
         menukort.add(new Pizza(1, "Vesuvio", 57.0, "tomatsauce, ost, skinke og oregano"));
         menukort.add(new Pizza(2, "Amerikaner", 53.0, "tomatsauce, ost, oksefars og oregano"));
         menukort.add(new Pizza(3, "Cacciatore", 57.0, "tomatsauce, ost, pepperoni og oregano"));
-        String[] input = {"1","2"};
+        String[] input = {"2", "2", "2", "AnyKey"};
         FakeUI ui = new FakeUI(input);
         Controller ctrl = new Controller(ui, menukort);
 
         //act
         ctrl.newOrder();
-        
 
         //assert
-        assertEquals("2", ui.output.get(1));
-        assertTrue(ui.output.get(2).contains("Vesuvio"));
-        
-        
-        //act 2
-        ctrl.newOrder();
-        
-        //assert 2
-       
-        assertEquals("3", ui.output.get(4));
-        assertTrue(ui.output.get(5).contains("Amerikaner"));
-        assertTrue(ctrl.getActiveOrders().size() == 2);
-        assertEquals("Amerikaner", ctrl.getActiveOrders().get(1).getPizza().getPizzaName());
+        assertTrue(ui.output.get(0).contains("Skriv hvilket pizzanummer kunden har bestilt"));
+        assertTrue(ui.output.get(1).contains("Skriv hvor mange af pizzaen kunde har bestilt"));
+        assertTrue(ui.output.get(2).contains("Har kunden bestilt flere pizzaer?"));
+        assertTrue(ui.output.get(10).contains("Ordrernummeret er"));
+        assertTrue(ui.output.get(12).contains("Amerikaner"));
+        assertTrue(ctrl.getActiveOrders().size() == 1);
+        assertTrue(ctrl.getActiveOrders().get(0).getPizzaList().size() == 2);
+        assertEquals("Amerikaner", ctrl.getActiveOrders().get(0).getPizzaList().get(0).getPizzaName());
+        assertEquals("Amerikaner", ctrl.getActiveOrders().get(0).getPizzaList().get(1).getPizzaName());
     }
-     */
+
+    @Test
+    public void opretBestillingToForskelligePizza() {
+        ArrayList<Pizza> menukort = new ArrayList();
+        menukort.add(new Pizza(1, "Vesuvio", 57.0, "tomatsauce, ost, skinke og oregano"));
+        menukort.add(new Pizza(2, "Amerikaner", 53.0, "tomatsauce, ost, oksefars og oregano"));
+        menukort.add(new Pizza(3, "Cacciatore", 57.0, "tomatsauce, ost, pepperoni og oregano"));
+        String[] input = {"2", "1", "1", "1", "1", "2", "AnyKey"};
+        FakeUI ui = new FakeUI(input);
+        Controller ctrl = new Controller(ui, menukort);
+
+        //act
+        ctrl.newOrder();
+
+        //assert
+        assertTrue(ui.output.get(0).contains("Skriv hvilket pizzanummer kunden har bestilt"));
+        assertTrue(ui.output.get(1).contains("Skriv hvor mange af pizzaen kunde har bestilt"));
+        assertTrue(ui.output.get(2).contains("Har kunden bestilt flere pizzaer?"));
+        assertTrue(ui.output.get(5).contains("Skriv hvilket pizzanummer kunden har bestilt"));
+        assertTrue(ui.output.get(6).contains("Skriv hvor mange af pizzaen kunde har bestilt"));
+        assertTrue(ui.output.get(7).contains("Har kunden bestilt flere pizzaer?"));
+        assertTrue(ui.output.get(15).contains("Ordrernummeret er"));
+        assertTrue(ui.output.get(17).contains("Amerikaner"));
+        assertTrue(ctrl.getActiveOrders().size() == 1);
+        assertTrue(ctrl.getActiveOrders().get(0).getPizzaList().size() == 2);
+        assertEquals("Amerikaner", ctrl.getActiveOrders().get(0).getPizzaList().get(0).getPizzaName());
+        assertEquals("Vesuvio", ctrl.getActiveOrders().get(0).getPizzaList().get(1).getPizzaName());
+    }
+
+    @Test
+    public void opretToBestillingerAfEnPizza() {
+        ArrayList<Pizza> menukort = new ArrayList();
+        menukort.add(new Pizza(1, "Vesuvio", 57.0, "tomatsauce, ost, skinke og oregano"));
+        menukort.add(new Pizza(2, "Amerikaner", 53.0, "tomatsauce, ost, oksefars og oregano"));
+        menukort.add(new Pizza(3, "Cacciatore", 57.0, "tomatsauce, ost, pepperoni og oregano"));
+        String[] input = {"2", "1", "2", "Anykey", "1", "1", "2", "AnyKey"};
+        FakeUI ui = new FakeUI(input);
+        Controller ctrl = new Controller(ui, menukort);
+
+        //act
+        ctrl.newOrder();
+        ctrl.newOrder();
+
+        //assert
+        assertTrue(ui.output.get(0).contains("Skriv hvilket pizzanummer kunden har bestilt"));
+        assertTrue(ui.output.get(1).contains("Skriv hvor mange af pizzaen kunde har bestilt"));
+        assertTrue(ui.output.get(2).contains("Har kunden bestilt flere pizzaer?"));
+        assertTrue(ui.output.get(10).contains("Ordrernummeret er"));
+        assertTrue(ui.output.get(12).contains("Amerikaner"));
+        assertTrue(ui.output.get(13).contains("Press any key to return to the main menu"));
+        assertTrue(ui.output.get(26).contains("Vesuvio"));
+        assertTrue(ctrl.getActiveOrders().size() == 2);
+        assertTrue(ctrl.getActiveOrders().get(0).getPizzaList().size() == 1);
+        assertTrue(ctrl.getActiveOrders().get(1).getPizzaList().size() == 1);
+        assertEquals("Amerikaner", ctrl.getActiveOrders().get(0).getPizzaList().get(0).getPizzaName());
+        assertEquals("Vesuvio", ctrl.getActiveOrders().get(1).getPizzaList().get(0).getPizzaName());
+    }
+
 }
