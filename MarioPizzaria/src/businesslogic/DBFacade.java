42185db4c00e7e9ed93e8e2874c253a5ac00ab1b
Jens <Jens@10.50.137.8>
2019-04-09 10:02:18 +0200

    db

---
+++ MarioPizzaria/src/businesslogic/DBFacade.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslogic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author Jens
+ */
+public class DBFacade{
+    private final Connection connect;
+    private Statement statement;
+    
+    public DBFacade() throws SQLException{
+        connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Marios_Pizza", "root", "1234");
+        statement = connect.createStatement();
+    }
+    
+    public void insert(String values, String table, String columns) throws SQLException{
+        statement.executeUpdate("INSERT INTO " + table + " " + columns + " VALUES " + values);
+    }
+}
