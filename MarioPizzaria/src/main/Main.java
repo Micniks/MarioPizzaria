ed81e1741d1752045257022331b3b1da0c22c96d
nicolaigregersen <nicolaigregersen@10.50.137.112>
2019-04-11 16:06:34 +0200

    refactoring

--- MarioPizzaria/src/main/Main.java
+++ MarioPizzaria/src/main/Main.java
@@ -5,19 +5,15 @@ import businesslogic.DBFacade;
 import businesslogic.Facade;
 import businesslogic.FileFacade;
 import businesslogic.Menu;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import presentation.SystemUI;
 import presentation.UI;
-import java.util.ArrayList;
 
-/**
- *
- * @author Michael N. Korsgaard
- */
+/* @author Michael N. Korsgaard
+ * @author Nicolai Gregersen
+ * @author Jens Brønd
+ * @author Oscar Laurblad*/
+
 public class Main {
 
     public static void main(String[] args) throws SQLException {
@@ -28,7 +24,7 @@ public class Main {
         UI ui = new SystemUI();
         Facade facade;
         int facadeType = ui.askFacade();
-        if (facadeType == 1){
+        if (facadeType == 1) {
             facade = new DBFacade(ui.getPassword());
         } else {
             facade = new FileFacade();
@@ -36,29 +32,7 @@ public class Main {
 
         Controller ctrl = new Controller(ui, menu.getMenu(), facade);
         ctrl.start();
-        
-        //DBFacade db = new DBFacade();
-        //ArrayList test = db.readPizzaQtyFromDatabase();
-        //System.out.println(test.get(2));
-        //System.out.println(test.get(3));
-       //db.insertOrders("33", "Orders", "OrderPrice");
-//        db.select("Pizza");
 
-        /*     Til DatabaseFacade
-        String user = "root";
-        String password = "Gunstar1";
-        String IP = "localhost";
-        String PORT = "3306";
-        String DATABASE = "mario";
-        String serverTime = "serverTimezone=UTC";
-        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE+"?"+serverTime;
-        Connection connection = DriverManager.getConnection(url, user, password);
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("SELECT * FROM Pizza");
-        while (result.next()) {
-            int resultat = result.getInt("pizzaNo");
-            System.out.println(resultat);
-         */
     }
 
 }
